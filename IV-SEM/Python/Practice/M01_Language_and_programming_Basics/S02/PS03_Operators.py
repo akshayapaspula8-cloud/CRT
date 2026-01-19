77e472f652aa22d1af836a4c54d4f12671ab5c40
akshayapaspula8-cloud <akshayapaspula8@gmail.com>
2026-01-19 15:06:28 +0530

    M01

---
+++ IV-SEM/Python/Practice/M01_Language_and_programming_Basics/S02/PS03_Operators.py
@@ -0,0 +1,22 @@
+x = 7 
+y = 13 
+print(x & y)
+print(x / y)
+print(x ^ y)
+print(~x)
+print(x >> 2)
+print (x << 2)
+
+#Membership operators 
+print(100 in [10 , 20 , 30 ,40])
+print("on" in "Python")
+
+#Identity operator 
+l1 = [ 1 , 2 , 3]
+l2 = [1 , 2 , 3]
+print(l1 is l2) 
+x = 10 
+y = 20 
+z = 10 
+print(x is y)
+print(x is z)
