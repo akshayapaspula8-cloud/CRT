4fb218c37ae537c24d4ad86db14638e2c293756d
akshayapaspula8-cloud <akshayapaspula8>
2026-03-08 23:55:50 +0530

    msg

---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S01/PS01_Time_Complexity.py
@@ -0,0 +1,12 @@
+'''ime Complexity
+ def: time complexity can be measure based upon the input
+ ex:
+  n=10
+  print(n)
+  o(1)-->Constant Time
+  o(n)-->single loop
+  o(n^2)-->Two loop
+  o(log n)-->Binary search
+  o(n log n)-->linearithmetic
+  o(2^n)-->Recursions
+  '''
